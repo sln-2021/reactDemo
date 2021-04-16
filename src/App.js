a0b29bffb7fe4b70fd09406e0b70474af6c727d7
jxxpglll@163.com <jxxpglll@163.com>
2021-04-16 09:21:23 +0800

    修改好react-router路由跳转

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-import { Link,Route,  } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/home';
 import Welcome from './components/welcome';
 import Detail from './components/detail';
-import { createHashHistory } from 'history';
-import { Router } from 'react-router';
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
@@ -14,66 +12,66 @@ const { Header, Content, Footer, Sider } = Layout;
 class App extends React.Component {
   state = {
     collapsed: false,
+    pathLocation: '',
   };
 
   onCollapse = (collapsed) => {
-    console.log(collapsed);
     this.setState({ collapsed });
   };
 
-  componentDidMount() {
-    console.log('1111');
-  }
   handleClick = (e) => {
-    console.log('click ', e);
+    this.setState({ pathLocation: e.key });
   };
+
   render() {
-    const { collapsed } = this.state;
-    console.log('2222');
+    const { collapsed, pathLocation } = this.state;
     return (
       <Router>
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} theme='light'>
-              <div style={{ height: 32, margin: 16, background: '#EFF8FB' }} />
-              <Menu theme='light' defaultSelectedKeys={['home']} mode='inline' onClick={this.handleClick}>
-                <Menu.Item key='home' icon={<PieChartOutlined />}>
-                  <Link to='/'>home</Link>
-                </Menu.Item>
-                <Menu.Item key='welcome' icon={<DesktopOutlined />}>
-                  <Link to='/welcome'>welcome</Link>
-                </Menu.Item>
-                <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
-                  <Menu.Item key='3'>Tom</Menu.Item>
-                  <Menu.Item key='4'>Bill</Menu.Item>
-                  <Menu.Item key='5'>Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
-                  <Menu.Item key='6'>Team 1</Menu.Item>
-                  <Menu.Item key='8'>Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key='9' icon={<FileOutlined />}>
-                  Files
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout className='site-layout'>
-              <Header className='site-layout-background' style={{ padding: 0 }} />
-              <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className='site-layout-background' style={{ padding: 24, minHeight: 360 }}>
-                  {/* <Route exact path='/detail' component={Detail} />
-                  <Route exact path='/home' component={Home} />
-                  <Route exact path='/welcome' component={Welcome} /> */}
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-            </Layout>
-                  
+        <Layout style={{ minHeight: '100vh' }}>
+          {/* Sider侧边栏 */}
+          <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} theme='light'>
+            {/* logo图片 */}
+            <div style={{ height: 32, margin: 16, background: '#EFF8FB' }} />
+            {/* 左侧导航区域 */}
+            <Menu theme='light' defaultSelectedKeys={['home']} mode='inline' onClick={this.handleClick}>
+              <Menu.Item key='home' icon={<PieChartOutlined />}>
+                <Link to='/home'>home</Link>
+              </Menu.Item>
+              <Menu.Item key='welcome' icon={<DesktopOutlined />}>
+                <Link to='/welcome'>welcome</Link>
+              </Menu.Item>
+              <Menu.Item key='detail' icon={<UserOutlined />} title='User'>
+                <Link to='/detail'>detail</Link>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          {/* 右侧connect内容 */}
+          <Layout className='site-layout'>
+            <Header className='site-layout-background' style={{ padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>主页</Breadcrumb.Item>
+                <Breadcrumb.Item>{`${pathLocation}`}</Breadcrumb.Item>
+              </Breadcrumb>
+              <div className='site-layout-background' style={{ padding: 24, minHeight: 360 }}>
+                {/* 组件导航路由 */}
+                <Switch>
+                  <Route path='/home'>
+                    <Home />
+                  </Route>
+                  <Route path='/detail'>
+                    <Detail />
+                  </Route>
+                  <Route path='/welcome'>
+                    <Welcome />
+                  </Route>
+                </Switch>
+              </div>
+            </Content>
           </Layout>
-        </Router>
+                
+        </Layout>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,3 @@ import App from './App';
 import 'antd/dist/antd.css';
 // 用ReactDOM.render来将元素渲染到页面中
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
